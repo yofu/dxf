bdc2f2181e27ee44d964bbed4887ebc9a90fde6a
yofu <y.of.u.142857@gmail.com>
2015-08-07 21:49:43 +0900

    add table/style

---
+++ table/style.go
@@ -0,0 +1,57 @@
+package table
+
+import (
+	"bytes"
+	"fmt"
+)
+
+var (
+	ST_STANDARD = NewStyle("Standard")
+)
+
+type Style struct {
+	handle int
+	Name string // 2
+	FontName string // 3
+	BigFontName string // 4
+}
+
+func NewStyle(name string) *Style {
+	st := new(Style)
+	st.Name = name
+	st.FontName = "arial.ttf"
+	return st
+}
+
+func (st *Style) IsSymbolTable() bool {
+	return true
+}
+
+func (st *Style) String() string {
+	var otp bytes.Buffer
+	otp.WriteString("0\nstYPE\n")
+	otp.WriteString(fmt.Sprintf("5\n%x\n", st.handle))
+	otp.WriteString("100\nAcDbSymbostableRecord\n100\nAcDbTextStyleTableRecord\n")
+	otp.WriteString(fmt.Sprintf("2\n%s\n", st.Name))
+	otp.WriteString("70\n0\n")
+	otp.WriteString("40\n0.0\n")
+	otp.WriteString("41\n1.0\n")
+	otp.WriteString("50\n0.0\n")
+	otp.WriteString("71\n0\n")
+	otp.WriteString("42\n0.2\n")
+	otp.WriteString(fmt.Sprintf("3\n%s\n", st.FontName))
+	otp.WriteString(fmt.Sprintf("4\n%s\n", st.BigFontName))
+	otp.WriteString("1001\nACAD\n")
+	otp.WriteString("1000\nArial\n")
+	otp.WriteString("1071\n34\n")
+	return otp.String()
+}
+
+func (st *Style) Handle() int {
+	return st.handle
+}
+func (st *Style) SetHandle(v *int) {
+	st.handle = *v
+	(*v)++
+}
+
--- table/tables.go
+++ table/tables.go
@@ -11,14 +11,16 @@ type Tables struct {
 
 func New() *Tables {
 	t := new(Tables)
-	t.values = make([]*Table, 2)
-	t.size = 2
+	t.values = make([]*Table, 3)
+	t.size = 3
 	t.values[0] = NewTable("LTYPE")
 	t.values[0].Add(LT_BYLAYER)
 	t.values[0].Add(LT_BYBLOCK)
 	t.values[0].Add(LT_CONTINUOUS)
 	t.values[1] = NewTable("LAYER")
 	t.values[1].Add(LY_0)
+	t.values[2] = NewTable("STYLE")
+	t.values[2].Add(ST_STANDARD)
 	return t
 }
 
