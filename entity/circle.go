d0b5732aeeb5863368aadb2f1843d0ee25df1f79
yofu <y.of.u.142857@gmail.com>
2015-08-10 22:37:55 +0900

    add Circle.Direction

--- entity/circle.go
+++ entity/circle.go
@@ -9,6 +9,7 @@ type Circle struct {
 	*entity
 	Center []float64 // 10, 20, 30
 	Radius float64   // 40
+	Direction []float64 // 210, 220, 230
 }
 
 func (c *Circle) IsEntity() bool {
@@ -20,6 +21,7 @@ func NewCircle() *Circle {
 		NewEntity(CIRCLE),
 		[]float64{0.0, 0.0, 0.0},
 		0.0,
+		[]float64{0.0, 0.0, 1.0},
 	}
 	return c
 }
@@ -32,5 +34,12 @@ func (c *Circle) String() string {
 		otp.WriteString(fmt.Sprintf("%d\n%f\n", (i+1)*10, c.Center[i]))
 	}
 	otp.WriteString(fmt.Sprintf("40\n%f\n", c.Radius))
+	for i := 0; i < 3; i++ {
+		otp.WriteString(fmt.Sprintf("%d\n%f\n", 200+(i+1)*10, c.Direction[i]))
+	}
 	return otp.String()
+}
+
+func (c *Circle) SetDirection(d []float64) {
+	c.Direction = d
 }
