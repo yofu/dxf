6a31a126a9dc146162c0fa285a140b20f60e5a9d
jeff <russoj88@gmail.com>
2018-11-29 18:08:10 -0800

    Move example to a test that can run and find the file naturally.  Add test for FromStringData to see output is the same.

---
+++ dxf_test.go
@@ -0,0 +1,37 @@
+package dxf
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/yofu/dxf/entity"
+)
+
+func TestFromStringData(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/point.dxf")
+	if err != nil {
+		log.Fatal("could not open file: ", err)
+	}
+	dr, err := FromStringData(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, e := range dr.Entities() {
+		if p, ok := e.(*entity.Point); ok {
+			fmt.Println(p.Coord)
+		}
+	}
+}
+func TestFromFile(t *testing.T) {
+	dr, err := FromFile("testdata/point.dxf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, e := range dr.Entities() {
+		if p, ok := e.(*entity.Point); ok {
+			fmt.Println(p.Coord)
+		}
+	}
+}
