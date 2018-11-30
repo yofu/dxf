686e14e9f753cc05d7ee339a72d8b5d263ab4773
jeff <russoj88@gmail.com>
2018-11-29 17:57:45 -0800

    Avoid package name collision

--- example/point/point.go
+++ example/point/point.go
@@ -9,11 +9,11 @@ import (
 )
 
 func main() {
-	dxf, err := dxf.Open("point.dxf")
+	dr, err := dxf.Open("point.dxf")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, e := range dxf.Entities() {
+	for _, e := range dr.Entities() {
 		if p, ok := e.(*entity.Point); ok {
 			fmt.Println(p.Coord)
 		}
